d580ab93b2c13ee39477d7798acd3cfbeaaf34d6
keithjohngates <keithjohngates@gmail.com>
2019-04-27 00:50:56 -0700

    header

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-  
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello there, welcome to our app!'
+    return render_template('header.html')
+    #return 'Hello there, welcome to our app!'
